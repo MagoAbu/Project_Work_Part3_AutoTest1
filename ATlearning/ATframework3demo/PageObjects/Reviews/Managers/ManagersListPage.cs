ad40d4db50f1bc9cedcd036f9bbd3ada03a978bc
magish_abu@mail.ru <magish_abu@mail.ru>
2025-05-01 19:06:02 +0200

    added logging when a manager is successfully added to the list of managers

--- ATlearning/ATframework3demo/PageObjects/Reviews/Managers/ManagersListPage.cs
+++ ATlearning/ATframework3demo/PageObjects/Reviews/Managers/ManagersListPage.cs
@@ -11,7 +11,14 @@ namespace ATframework3demo.TestCases.ProjectWorkPart3.Manager
             WebItem managerName = new WebItem($"//span[@class='main-grid-cell-content' and text()='{ManagerLastNameName}']",
                 "Строка с именем и фамилией в списке менеджеров");
             if (managerName.InnerText() != ManagerLastNameName)
-                Log.Error($"Менеджер не отображается в списке. Ожидалось, что в спике появится менеджер с именем {ManagerLastNameName}");
+            {
+                Log.Error($"Менеджер не отображается в списке. Ожидалось, что в списке появится менеджер с именем {ManagerLastNameName}");
+            }
+            else
+            {
+                Log.Info($"Менеджер с именем {ManagerLastNameName} отображается в списке менеджеров.");
+            }
+               
             return new ManagersListPage();
         }
     }
